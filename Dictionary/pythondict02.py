ccc18efc21c8a2a116d222612d85a80aded13471
Abinash Shrestha <abshrestha312@gmail.com>
2020-12-09 16:13:30 +0000

    join and split

---
+++ Dictionary/pythondict02.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# create a list called list1
+doglist = ["Tommy", "Bob", "Bruno"]
+# display doglist
+print(doglist)
+
+# create a new list called catlist
+catlist = ["Mimi", "Romi", "Lolo"]
+print(catlist)
+
+#use the append operation to append doglist by catlist
+doglist.append(catlist)
+#display the new appended list
+print(doglist)
+
+
+
+
+
+
+
+
---
+++ challengelist/complex01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# create a dog list called list1
+list1 = ["Tommy", "Bruno", "Jojo"]
+# display list1
+print(list1)
+
+# create a new cat list called list2
+list2 = ["Munu", "Nunu", "Sani"]
+#display list2
+print(list2)
+
+#use the append operation to append list1 by list2
+list1.append(list2)
+#display the new complex appended list1
+print(list1)
+
