c4fcbc20b81758fded195bcd0561f32fbdcfca62
Abinash Shrestha <abshrestha312@gmail.com>
2020-12-11 17:08:58 +0000

    Func_challenge

---
+++ func_challenge.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#create a function calle my_function
+
+def my_function(name):
+
+        for number in range(2):
+
+            print(f'Hello {name}!')
+    
+my_function('Mike')
+
+
+number = 2
+
+while number < 4 :
+    print(11*number)
+    # Increment `number` by 1
+    number = number+1
+
+
